83348d101c33608b2afd47015c2360c01aef06c5
Berk Yagmurlu <berk.yagmurluu@gmail.com>
2026-01-09 22:27:14 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const plugins = [react(), tailwindcss(), jsxLocPlugin()];
 
